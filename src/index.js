ee74f1dec9ee5446629030414a3b12ad8602f5d8
ContersK <luangonfernandes@outlook.com>
2024-12-10 14:44:43 -0300

    feat: initial config

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { server } from "./server/Server.js";
+
+const PORT = 3333;
+
+console.log('iniciando server...');
+
+server.listen(PORT, () => {
+    console.log(`server rodando na porta numero: ${PORT}`);
+}).on('error', (err) => {
+    console.log(`falha ao iniciar o server em: ${PORT}`, err.message);
+});
