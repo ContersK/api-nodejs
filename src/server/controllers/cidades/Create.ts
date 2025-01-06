0ebedb13ae5602fa18282a25683a1d596d61cbfc
ContersK <luangonfernandes@outlook.com>
2025-01-06 16:18:16 -0300

    Mensagem descritiva do commit

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -11,12 +11,12 @@ interface ICidade {
 export const createValidation = validation((getSchema) => ({
   body: getSchema<ICidade>(
     yup.object().shape({
-      nome: yup.string().required().min(3),
+      nome: yup.string().strict().required().min(3),
     })
   ),
 }));
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   console.log(req.body);
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("não implementado");
+  return res.status(StatusCodes.CREATED).json(1);
 };
