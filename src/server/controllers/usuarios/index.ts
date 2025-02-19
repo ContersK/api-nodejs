4c60e3e0f713eefd82df4b787f4f53dc73f61030
Conters <luangonfernandes@gmail.com>
2025-02-19 15:05:22 -0300

    feat: signIn and signUp

---
+++ src/server/controllers/usuarios/index.ts
@@ -0,0 +1,7 @@
+//import * as Create from './Create';
+//import * as GetByID from './GetByID';
+
+export const UsuariosController = {
+  // ...Create,
+  //...GetByID,
+};
---
+++ src/server/database/providers/usuarios/index.ts
@@ -0,0 +1,7 @@
+import * as Create from './Create';
+import * as getByEmail from './GetByEmail';
+
+export const UsuariosProvider = {
+  ...Create,
+  ...getByEmail,
+};
